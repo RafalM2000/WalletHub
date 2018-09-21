e0450baa26e18feb9469eeb47bb697b4ce0af1b3
Rafal <rmf@europe.com>
2018-09-21 07:04:50 +0200

    clearing not necessary lines in module two

--- src/app/moduleTwo/two.module.ts
+++ src/app/moduleTwo/two.module.ts
@@ -4,43 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
-// import { AppComponent } from '../app.component';
-// import { MenuComponent } from '../menu/menu.component';
-
 import { TwoComponent } from './two/two.component';
 
 import { UserService } from '../user.service';
-// import { HomeComponent } from './home/home.component';
-// import { OneDetailsComponent } from './one-details/one-details.component';
 
 const twoRoutes: Routes = [
-  // { path: 'login', component: LoginComponent },
-  // { path: 'home', component: HomeComponent },
-  // { path: 'one', component: OneComponent},
   { path: 'two', component: TwoComponent},
-  // { path: '', redirectTo: '/login', pathMatch: 'full'},
-  // { path: '**', redirectTo: '/login', pathMatch: 'full'}
 ]
-
-
 @NgModule({
   declarations: [
-    // AppComponent,
-    // LoginComponent,
-    // MenuComponent,
-    // OneComponent,
-    TwoComponent,
-    // HomeComponent,
-    // OneDetailsComponent
+    TwoComponent
   ],
   imports: [
     CommonModule,
-    // ReactiveFormsModule,
     FormsModule,
     RouterModule.forChild(twoRoutes),
     BrowserAnimationsModule
   ],
-  providers: [ UserService ],
-  bootstrap: []
+  providers: [ UserService ]
 })
 export class TwoModule { }
