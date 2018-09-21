1f9069123037f75313894e1eb55ba7e42e1ad15a
Rafal <rmf@europe.com>
2018-09-21 21:27:54 +0200

    login validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,6 +9,7 @@ import { UserService } from '../user.service'
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  messageValidatr;
 
   formModel: FormGroup = new FormGroup({
     user: new FormGroup({
@@ -23,7 +24,16 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit() {
+    let form = this.formModel.value;
+    if (form.user.login === "" || form.user.password === "") {
+      this.messageValidatr = 'Both: login and password should be filled in';
+      console.log(this.messageValidatr);
+      return;
+    }
     this._user.authentication(this.formModel.value);
+    if (this._user.authentication(this.formModel.value) === false) {
+      this.messageValidatr = 'Wrong login or password';
+    }
 
   }
 
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -12,12 +12,15 @@ export class UserService {
 
   constructor(private _router: Router) { }
 
-  authentication(data) { 
+  authentication(data): boolean { 
     //  console.log(data.user.login)
     if (data.user.login === this.userLogin && data.user.password === this.userPassword) {
       localStorage.setItem('login', 'Jon')
       localStorage.setItem('IsUsereAuthorised', 'Yes');
       this._router.navigate(["/home"]);
+    }
+    else {
+      return false;
     }
   }
 
