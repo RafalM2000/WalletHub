aee2e01cd9c48518f207dd18148a12921efa243b
Rafal <rmf@europe.com>
2018-09-22 19:13:14 +0200

    fadeIn home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fade', [
+      // state(),
+      transition('void => *', [
+        style({opacity: 0}),
+        animate(4000, style({opacity: 1}))
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
   isHidden: boolean = true;
