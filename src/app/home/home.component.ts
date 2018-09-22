9efb498b4d6aff33802a4a4ecf162971a6c99dc2
Rafal <rmf@europe.com>
2018-09-22 19:05:29 +0200

    Html attribute binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  isHidden: boolean = true;
+  buttonMessage: string = 'show me more';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  show() {
+    this.isHidden = !this.isHidden;
+    if (this.isHidden === true) {
+      this.buttonMessage = "show me more"
+    }
+    else {
+      this.buttonMessage = "hide below info"
+    }
+  }
 }
